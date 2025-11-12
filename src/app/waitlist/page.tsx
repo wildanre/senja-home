89080bc41e01269b06e5ffc0ae3b86682eefe886
wildanre <mws02505@gmail.com>
2025-11-12 18:56:33 +0700

    Add WaitlistPage component to display waitlist section
    - Introduced a new WaitlistPage component that renders the WaitlistSection.

---
+++ src/app/waitlist/page.tsx
@@ -0,0 +1,13 @@
+import WaitlistSection from "@/components/sections/waitlist";
+
+export default function WaitlistPage() {
+  return (
+    <div>
+      <div className="relative w-full overflow-hidden py-8 px-4">
+        <div className="max-w-7xl py-20  mx-auto">
+          <WaitlistSection />
+        </div>
+      </div>
+    </div>
+  );
+}
